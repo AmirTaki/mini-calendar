78d4d05d6322e99391b9ce13d3ce4ed444c57b7d
amir <amirtaki623@gmail.com>
2025-05-22 19:48:23 +0330

    js : getElementBy id

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const date = document.getElementById('date')
+const day = document.getElementById('day')
+const month = document.getElementById('month')
+const year = document.getElementById('year')
+
+
