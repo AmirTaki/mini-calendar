f67c5bb5d306966e69e28b4b0025a602cf30cdb3
amir <amirtaki623@gmail.com>
2025-05-22 19:56:13 +0330

    js : days & months

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ const year = document.getElementById('year')
 
 const today = new Date();
 
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 date.innerHTML = today.getDate()
 day.innerHTML = today.getDay()
 month.innerHTML = today.getMonth()
-year.innerHTML = today.getYear()
+year.innerHTML = today.getFullYear()
